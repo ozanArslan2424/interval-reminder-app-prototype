b7150aae3447b7593e69f3c0828d4bd231ed18ab
ozanArslan2424 <136094471+ozanArslan2424@users.noreply.github.com>
2023-10-10 15:32:58 +0300

    v0.0.3 small changes
    removed unnecessary component, added containers where needed, added css variables for better customization

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,20 @@
-import CardContent from "./CardContent"
-
+import './cardcontent.css'
 export default function Card() {
     return (
         <div className="card">
-            <h2 className="card-title">Title</h2>
+            <h2 className="card-title">Hatırlatıcını ayarla</h2>
             <h3 className="card-subtitle">Subtitle</h3>
-            <CardContent/>
-            <div className="interval-type">hour/day/week/month/year</div>
+            <div className="card-content">
+            <button className="minus button">-</button>
+            <div className="mid-container">
+            <div className="numbers-container">
+                <div className="numbers">31</div>
+                <div className="numbers-flip"></div>
+            </div>
+            <div className="interval-type">interval</div>
+            </div>
+            <button className="plus button">+</button>
+        </div>
         </div>
     )
 }
